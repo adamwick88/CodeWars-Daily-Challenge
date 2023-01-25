dcf8357af553c487179ea919846cdc7c76495a43
Adam <adamwick1988@gmail.com>
2023-01-25 17:47:40 -0600

    Adding Coding Challenge for the 25th

---
+++ Solution12.js
@@ -0,0 +1,20 @@
+/*Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+Note: only positive integers will be tested.
+*/
+
+
+function sumFromString(string) {
+    let sum = 0;
+    let currentNumber = '';
+    for (let char of string) {
+      if (!isNaN(char)) {
+        currentNumber += char;
+      } else {
+        sum += Number(currentNumber);
+        currentNumber = '';
+      }
+    }
+    sum += Number(currentNumber);
+    return sum;
+  }
