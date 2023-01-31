c64c0ce64152bbab56a16bbfb786408e29d21f82
Adam <adamwick1988@gmail.com>
2023-01-30 18:50:36 -0600

    Monday Coding Challenge

--- Solution03.js
+++ Solution03.js
@@ -11,5 +11,3 @@ function multipleOfIndex(array) {
     return array.filter((num, i) => num % i === 0);
   }
 
-
-  
---
+++ Solution06.js
@@ -0,0 +1,8 @@
+/*In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once. */
+
+let repeatOnce(string){
+    let once=['']
+    for(let i=0; i=>string.length++){
+     once=once + string[i] +string[i]   
+    }
+}
