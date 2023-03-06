e9b70a202c5c97fb092b5ddc1e1fcac55496f00b
Adam <adamwick1988@gmail.com>
2023-03-06 08:22:21 -0600

    updating code

---
+++ Solution016.js
@@ -0,0 +1,4 @@
+function evenNumbers(array,number){
+    return array.filter(n=>n%2===0).slice(-number)
+}
+
---
+++ Solution20.js
@@ -0,0 +1,6 @@
+function repeatString(string){
+    let newString=""
+    for(let i=0; i<=string.length; i++){
+        newString=newString +string[i]+string[i]
+    }
+}
