7563a82e72990daad46ffdb30babc84abc8cb405
Adam <adamwick1988@gmail.com>
2023-05-22 09:42:24 -0500

    updates

---
+++ Solution33.js
@@ -0,0 +1,54 @@
+function date(string){
+    return string.split(",")
+}
+
+function bandName(string){
+    if(string.chartAt(0)===string.charAt(-1)){
+        return string.toUpperCase(0)+string.slice(1)+string.slice(1);
+    } else reutrn
+}
+
+
+function dragon(bullet,dragon){
+    return bullets>= dragons * 2
+}
+
+function addLenngth(str){
+    return str.split(" ").map(word=> `${word} ${word.length}`)
+}
+
+function evensOnly(array,index){
+    return array.filter(x=>x%2===0)
+}
+
+
+function bandNameGenerator(str){
+    if(str.charAt(0)===str.charAt(str.length-1)){
+        return str.charAt(0).toUpperCase()+str.slice(1)+str.slice(1);
+    }else{
+        return "The " +str.charAt(0).toUpperCase()+str.slice(1);
+    }
+}
+
+function isLockNessMonster(s){
+    return s.includes('tree fiddy') || s.includes('3.50') || 
+}
+
+let stopwatch={}
+stopwatch.currentTime=12
+stopwatch.tellTime=function(time){
+
+}
+
+function addLength(str){
+    reutrn str.spliit(" ").map(word=> `${word} $` )
+}
+
+function sumMix(x){
+    sum=x.map(a=>+a).reduce((a,b))=>a+b)
+}
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((a,c)=>a+c)/marks.length)
+}
+
