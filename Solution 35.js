14806a64011637fb8549da3b857d0ec7167b6d79
Adam <adamwick1988@gmail.com>
2023-06-12 08:45:11 -0500

    updates

---
+++ Solution 35.js
@@ -0,0 +1,71 @@
+function noRepeats(array){
+    return array.filter(n=>arrayindexOf(n)===array.lastIndexOf(n)).reduce((a,c)=>a+c,0)
+};
+
+    return array.filter(n=>array.indexOf(n)===array.lastIndexOf(n)).reduce((a,c)=>a+c,0)
+    
+
+function addLength(str){
+    return str.split(" ").map(word => `${word} ${word.length}`)
+
+    return str.split(" ").map(word=> `{word} ${word.lgnth}`)
+{
+
+}
+
+function shortenToDate(longDate){
+    return longDate.split(",")}{0}
+}
+
+
+return array.filter(n=>array.lastIndOf(n)===array.lastIndeoxOf()).reduce()
+
+function bandNameGenerator(str){
+    if(str.charAt(0)===str.charAt(str.length-1)){
+        return str.charAt(0).toUpperCase().str.slice(1)+str.slice(1);
+    }else{
+        return "The"+ str.charAt(0).toUpperCase()+str.slice(1);
+    }
+}
+
+    if(str.charAt(0)===str.chatAt(-1){
+        return str.charAt(0)+str.slice(1)+str.slice(1)
+    }else{
+        return "The"+str.charAt(0).toUpperCase(0)
+    }
+
+function evenNumbersOnly(array,number){
+    return array.filter(n=>n%2===0).slice(-number)
+}
+
+return array.filter(n=>n%2==0).slice(-number)
+
+array.filter(n=>n%2===0).slice(-number)
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((a,c)=>a+c)/marks.length)
+    return Math.floor(marks.reduce((a,c)=>a+c)/marks.length)
+}
+array.map(a=>a+)
+
+function sumMix(x){
+    sum=x.map(a=>+a).reduce((a,b)=>a+b)
+    return sum
+}
+
+function returnIndexONly(array){
+    return array.fiter((num,i)=>num%i===0);
+}
+return array.filter((num,i)=>num%i===0)
+
+function findDeletedNumber(arr,mixArr){
+    if(arr.length===mixArr.length){
+        return 0
+    }else
+}
+
+function hero(bullets, dragons){
+    return bullets>=dragon *2
+}
+
+function seach(budget,)
